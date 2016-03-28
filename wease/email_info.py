3475b6007d0adce4876bc13072dd8463878e772a
Daniel Micaletti <dmic23@gmail.com>
2016-03-28 13:35:55 -0700

    Delete email_info.py

--- wease/email_info.py
+++
@@ -1,8 +0,0 @@
-# For gmail and google apps
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST= 'smtp.gmail.com'
-EMAIL_HOST_USER = 'weasereg@gmail.com'
-EMAIL_HOST_PASSWORD = 'WeASeAdmin2015'
-EMAIL_PORT = 587
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
